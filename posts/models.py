e0cab5dba5c25bea5fc07becbd8627e56c5d03d5
unluckmeba <jantayt2006@gmail.com>
2023-02-20 17:24:12 +0600

    'hw2-3'

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+
 
 # Create your models here.
+
+
+# Create your models here.
+class Hashtag(models.Model):
+    title = models.CharField(max_length=50)
+    posts = models.ManyToManyField('Post', null=True, blank=True)
+
+    def str(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+    likes = models.IntegerField()
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import redirect, HttpResponse
 from datetime import date
+from django.views.generic import ListView, DetailView, CreateView
+from posts.models import *
 
 
 # Create your views here.
@@ -17,6 +19,17 @@ def now_date_view(request):
         return HttpResponse(f'Now date: {date.today()}')
 
 
-def goodbye_view(request):
+def hashtags_view(request):
     if request.method == 'GET':
-        return HttpResponse('Goodbye user!')
+        data = {
+            'hashtags': Hashtag.objects.all()
+        }
+        return render(request, 'posts/hashtags.html', context=data)
+
+
+def posts_view(request):
+    if request.method == 'GET':
+        data = {
+            'posts': Post.objects.all()
+        }
+        return render(request, 'posts/posts.html', context=data)
